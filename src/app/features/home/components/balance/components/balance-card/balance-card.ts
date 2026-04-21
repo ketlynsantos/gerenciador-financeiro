a13b4b33642e3f55af91ca63ba6838bfd48d09e4
Ketlyn Santos <ketlyncastro2004@gmail.com>
2026-04-21 13:17:58 -0300

    fix: corrige bug do saldo zerado

---
+++ src/app/features/home/components/balance/components/balance-card/balance-card.ts
@@ -0,0 +1,33 @@
+import { Component, computed, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+type CardType = 'income' | 'outcome' | 'balance'
+type ValueCssClass = 'income' | 'outcome' | 'zero';
+
+@Component({
+  selector: 'app-balance-card',
+  imports: [MatCardModule],
+  templateUrl: './balance-card.html',
+  styleUrl: './balance-card.scss',
+})
+export class BalanceCard {
+  type = input.required<CardType>()
+  label = input.required<string>()
+  value = input.required<number>()
+
+  cssClass = computed<ValueCssClass>(() => {
+    if (this.type() === 'income') {
+      return 'income'
+    }
+
+    if (this.type() === 'outcome') {
+      return 'outcome'
+    }
+
+    if (this.value() === 0) {
+      return 'zero'
+    }
+
+    return this.value() > 0 ? 'income' : 'outcome'
+  })
+}
