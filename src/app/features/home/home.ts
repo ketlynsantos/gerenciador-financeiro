635a26c119f8fa8b240af68d748e9952915d2006
Ketlyn Santos <ketlyncastro2004@gmail.com>
2026-07-20 21:23:36 -0300

    feat: recupera dados usando TransactionService

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Balance } from './components/balance/balance';
 import { TransactionItem } from './components/transaction-item/transaction-item';
 import { Transaction } from '../../shared/transaction/interfaces/transaction';
-import { TransactionType } from '../../shared/transaction/enums/transaction-type';
 import { NoTransactions } from "./components/no-transactions/no-transactions";
+import { TransactionsService } from '../../shared/transaction/services/transactions';
 
 @Component({
   selector: 'app-home',
@@ -11,10 +11,18 @@ import { NoTransactions } from "./components/no-transactions/no-transactions";
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
-  transactions = signal<Transaction[]>([
-    // { title: 'Salário', value: 100, type: TransactionType.INCOME },
-    // { title: 'VA', value: 50, type: TransactionType.INCOME },
-    // { title: 'Aluguel', value: 150, type: TransactionType.OUTCOME },
-  ])
+export class Home implements OnInit {
+  private transactionsService = inject(TransactionsService)
+
+  transactions = signal<Transaction[]>([])
+
+  ngOnInit(): void {
+    this.getTransactions()
+  }
+  
+  private getTransactions() {
+    this.transactionsService.getAll().subscribe({
+      next: (response) => this.transactions.set(response)
+    })
+  }
 }
