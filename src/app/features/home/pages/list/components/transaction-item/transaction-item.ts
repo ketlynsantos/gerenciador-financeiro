033a2a02f94ae5426f08d8d158c94244320751bc
Ketlyn Santos <ketlyncastro2004@gmail.com>
2026-08-03 11:47:13 -0300

    refactor: move código do componente Home para dentro da pasta features/home/pages/list

--- src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
+++ src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
@@ -1,8 +1,8 @@
 import { Component, input, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { Transaction } from '../../../../shared/transaction/interfaces/transaction';
 import { TransactionValue } from './components/transaction-value/transaction-value';
+import { Transaction } from '../../../../../../shared/transaction/interfaces/transaction';
 
 @Component({
   selector: 'app-transaction-item',
--- src/app/features/home/pages/list/list.component.ts
+++ src/app/features/home/pages/list/list.component.ts
@@ -1,22 +1,22 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { Balance } from './components/balance/balance';
-import { TransactionItem } from './components/transaction-item/transaction-item';
-import { Transaction } from '../../shared/transaction/interfaces/transaction';
-import { NoTransactions } from "./components/no-transactions/no-transactions";
-import { MatAnchor, MatButton, MatButtonModule } from "@angular/material/button";
+import { Component, inject, signal } from '@angular/core';
+import { Transaction } from '../../../../shared/transaction/interfaces/transaction';
+import { TransactionsService } from '../../../../shared/transaction/services/transactions.service';
 import { Router, RouterLink } from '@angular/router';
-import { TransactionsService } from '../../shared/transaction/services/transactions.service';
-import { FeedbackService } from '../../shared/feedback/services/feedback.service';
-import { MatDialog } from '@angular/material/dialog';
-import { ConfirmationDialogService } from '../../shared/dialog/confirmation/services/confirmation-dialog.service';
+import { ConfirmationDialogService } from '../../../../shared/dialog/confirmation/services/confirmation-dialog.service';
+import { FeedbackService } from '../../../../shared/feedback/services/feedback.service';
+import { Balance } from './components/balance/balance';
+import { MatAnchor, MatButtonModule } from '@angular/material/button';
+import { NoTransactions } from './components/no-transactions/no-transactions';
+import { TransactionItem } from './components/transaction-item/transaction-item';
+import { TransactionsContainerComponent } from './components/transactions-container/transactions-container.component';
 
 @Component({
-  selector: 'app-home',
-  imports: [Balance, TransactionItem, NoTransactions, MatAnchor, MatButtonModule, RouterLink],
-  templateUrl: './home.html',
-  styleUrl: './home.scss',
+  selector: 'app-list',
+  imports: [Balance, TransactionItem, NoTransactions, MatAnchor, MatButtonModule, RouterLink, TransactionsContainerComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
 })
-export class Home implements OnInit {
+export class ListComponent {
   private transactionsService = inject(TransactionsService)
   private router = inject(Router)
   private feedbackService = inject(FeedbackService)
