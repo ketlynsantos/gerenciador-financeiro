19c0d4fdfad80a3952e8a36a39ba0896ca38e183
Ketlyn Santos <ketlyncastro2004@gmail.com>
2026-08-04 14:51:15 -0300

    refactor: cria função para gerar token genérica

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -5,13 +5,24 @@ import { UserCredentials } from '../interfaces/user-credentials';
 import { AuthTokenResponse } from '../interfaces/auth-token-response';
 import { User } from '../interfaces/user';
 
+function generateToken(): string {
+  let token = ''
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  for (let i = 0; i < 20; i++) {
+    token += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+
+  return token
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   login(payload: UserCredentials): Observable<AuthTokenResponse> {
     if (payload.user === 'admin' && payload.password === '123') {
-      return of({ token: 'fake-token' })
+      return of({ token: generateToken() })
     }
     return throwError(() => new HttpErrorResponse({ status: 401, statusText: 'Unauthorized' }))
   }
