6ac1bee531ca7d0ded072af7f4a4eca7f8273cab
Ketlyn Santos <ketlyncastro2004@gmail.com>
2026-08-04 17:22:58 -0300

    feat: impede do botão logout aparecer quando o usuario nao estiver logado

--- src/app/core/layout/components/header/header.ts
+++ src/app/core/layout/components/header/header.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core'
+import { Component, computed, inject } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { LogoutFacadeService } from '../../../auth/facades/logout-facade.service';
 import { Router } from '@angular/router';
+import { LoggedInUserStoreService } from '../../../auth/stores/logged-in-user-store.service';
 
 @Component({
   selector: 'app-header',
@@ -13,6 +14,9 @@ import { Router } from '@angular/router';
 export class Header {
   private readonly logoutFacadeService = inject(LogoutFacadeService)
   private readonly router = inject(Router)
+  private readonly loggedInUserStore = inject(LoggedInUserStoreService)
+
+  isLoggedIn = computed(() => this.loggedInUserStore.isLoggedIn())
   
   logout() {
     this.logoutFacadeService.logout().subscribe({
