2cf8c905b061e3c6b66bb20a2b81bc9d7d0d6206
Ketlyn Santos <ketlyncastro2004@gmail.com>
2026-08-04 16:38:16 -0300

    refactor: melhora registro de providers com a função provideCore

---
+++ src/app/core/provide-core.ts
@@ -0,0 +1,18 @@
+import { makeEnvironmentProviders } from "@angular/core";
+import { provideAuth } from "./auth/provide-auth";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarConfig } from "@angular/material/snack-bar";
+import { provideEnvironmentNgxMask } from "ngx-mask";
+import { setAuthTokenInterceptor } from "./auth/interceptors/set-auth-token-interceptor";
+
+export function provideCore() {
+    return makeEnvironmentProviders([
+        provideAuth(),
+        provideHttpClient(withInterceptors([setAuthTokenInterceptor])),
+        provideEnvironmentNgxMask({ thousandSeparator: '.', decimalMarker: ',' }),
+        {
+            provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+            useValue: { verticalPosition: 'top', horizontalPosition: 'center', duration: 3000 } as MatSnackBarConfig
+        },
+    ])
+}
