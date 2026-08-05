6ae678a6da8a83cff10901290d302d21267678af
Ketlyn Santos <ketlyncastro2004@gmail.com>
2026-08-05 10:47:33 -0300

    feat: cria diretiva para abstrair classe que da cor ao botão

--- src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
+++ src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
@@ -3,10 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { TransactionValue } from './components/transaction-value/transaction-value';
 import { Transaction } from '@shared/transaction/interfaces/transaction';
+import { CustomColorDirective } from '@shared/material/buttons/directives/custom-color.directive';
 
 @Component({
   selector: 'app-transaction-item',
-  imports: [MatCardModule, MatButtonModule, TransactionValue],
+  imports: [MatCardModule, MatButtonModule, TransactionValue, CustomColorDirective],
   templateUrl: './transaction-item.html',
   styleUrl: './transaction-item.scss',
 })
---
+++ src/app/shared/material/buttons/directives/custom-color.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, effect, ElementRef, inject, input, Renderer2 } from '@angular/core';
+
+type ColorType = 'error'
+
+@Directive({
+  selector: '[matButton]'
+})
+export class CustomColorDirective {
+  private readonly elementRef = inject(ElementRef)
+  // Manipular o DOM de forma abstrata
+  private readonly renderer2 = inject(Renderer2)
+
+  color = input<ColorType>(undefined, { alias: 'matButtonColor'})
+
+  constructor() {
+    effect(() => { 
+      if (this.color()) {
+        this.renderer2.addClass(this.elementRef.nativeElement, `btn-${this.color()}`)
+      }
+    })
+  }
+
+}
